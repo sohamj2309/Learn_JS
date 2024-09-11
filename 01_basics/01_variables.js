3a3d89c0ea03ea2c52914801828ede541b358e8a
sohamj2309 <117424889+sohamj2309@users.noreply.github.com>
2024-09-11 09:20:50 +0000

    Learned about variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553 //Const does not change
+let accountemail = "soham@google.com" //
+var accountPassword = "12345"
+accountCity="Nashik"
+
+console.table([accountId,accountPassword,accountemail,accountCity])
+//To print everything together
+
+//var has scope problem--> Block Scope and Functional Scope
+//use let and const
