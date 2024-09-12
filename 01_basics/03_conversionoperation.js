06d0512e5010abc2f18f089542f3e5e879247d72
sohamj2309 <117424889+sohamj2309@users.noreply.github.com>
2024-09-12 06:49:59 +0000

    Learned How to Convert

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,18 @@
+let score ="23"
+console.log(typeof score);
+let valueinNumber=Number(score)
+console.log(typeof valueinNumber);
+//NAN --> Not a Number
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
