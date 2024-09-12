e8c0195da97e97411a682e0aca745b3ec2e54f29
sohamj2309 <117424889+sohamj2309@users.noreply.github.com>
2024-09-12 07:25:27 +0000

    Learned Conversion Operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -16,3 +16,23 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
+/ console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
