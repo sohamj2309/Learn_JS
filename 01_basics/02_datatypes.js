3e3cc224c8c4abf53dfe09e1b5a297cdb8197041
sohamj2309 <117424889+sohamj2309@users.noreply.github.com>
2024-09-12 06:37:40 +0000

    Learned Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treats all JS code as newer version
+//alert works in browser and not in Nodejs
+//alert(3+3)
+console.log(3+3)
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+let age= 5
+console.log(typeof age);
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
